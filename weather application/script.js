f8b1387898b1eb3f6dc39f8929d0dda8d5334a9e
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-01-10 13:36:41 +0530

    added try and catch block for handling error

--- weather application/script.js
+++ weather application/script.js
@@ -1,14 +1,22 @@
 
 const API_key="251f938559c6bc8dfef291e571e5a97b";
 async function  showWeather(){
-    const lat=30.6510;
-    let lon=76.3293;
-       
-    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`);
-    let data=await response.json();
-    console.log(data);
-    let newPara=document.createElement('p');
-    newPara.textContent=    ` ${data?.main?.temp.toFixed(2)}`;
-    document.body.appendChild(newPara);
+    try{
+        const lat=30.6510;
+        let lon=76.3293;
+           
+        const response= await fetch(`https://api.openweatherp.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`);
+        let data=await response.json();
+        console.log(data);
+        let newPara=document.createElement('p');
+        newPara.textContent=    ` ${data?.main?.temp.toFixed(2)}`;
+        document.body.appendChild(newPara);
+
+    }
+    catch(err){
+        console.log("this is error")
+
+    }
+   
 }
 
